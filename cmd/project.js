628740c824ffab773fd4cee2b68ff70596505172
Steve Belovarich <steveblue@gmail.com>
2020-01-30 09:44:59 -0800

    FIXED issues with scaffold

--- cmd/project.js
+++ cmd/project.js
@@ -1,15 +1,42 @@
 import chalk from 'chalk';
 import { exec } from 'child_process';
 import { resolve } from 'path';
+import { readFile, writeFile } from 'fs';
 
+// TODO: resolve callback hell
 function project(name) {
     process.stdout.write(chalk.yellow(`\nBootstrapping ${name}`));
     exec(`git clone https://github.com/steveblue/react-starter.git ${name}`, {}, (error, stdout, stderr) => {
         process.stdout.write(chalk.blue(`\nInstalling dependencies...`));
-        exec(`yarn install`, { cwd: resolve(`./${name}`) }, (error, stdout, stderr) => {
-            process.stdout.write(chalk.green(`\nInstallation complete\n`));
+        readFile(resolve(`./${name}/package.json`), 'utf8', (err, pkg) => {
+            if (err) {
+                throw err;
+            } else {
+                pkg = JSON.parse(pkg);
+                pkg.name = name;
+                pkg.version = '1.0.0';
+                pkg = JSON.stringify(pkg, null, 4);
+                writeFile(resolve(`./${name}/package.json`), pkg, err => {
+                    if (err) {
+                        throw err;
+                    } else {
+                        exec(`rm -rf .git`, { cwd: resolve(`./${name}`) }, (err, stdout, stderr) => {
+                            exec(`git init`, { cwd: resolve(`./${name}`) }, (err, stdout, stderr) => {
+                                exec(`yarn install`, { cwd: resolve(`./${name}`) }, (err, stdout, stderr) => {
+                                    if (err) {
+                                        throw err;
+                                    } else {
+                                        process.stdout.write(chalk.green(`\nInstallation complete\n`));
+                                    }
+                                });
+                            });
+                        });
+                    }
+                });
+            }
+
         })
-    })
+    });
 }
 
 export { project };
